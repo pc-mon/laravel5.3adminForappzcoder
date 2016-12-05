7e9e3d065487d2e376b64e1328190fc974d04622
pc-moon <pc-moon@hotmail.com>
2016-12-06 00:41:28 +0300

    try the system

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('admin', 'Admin\AdminController@index');
+Route::get('admin/give-role-permissions', 'Admin\AdminController@getGiveRolePermissions');
+Route::post('admin/give-role-permissions', 'Admin\AdminController@postGiveRolePermissions');
+Route::resource('admin/roles', 'Admin\RolesController');
+Route::resource('admin/permissions', 'Admin\PermissionsController');
+Route::resource('admin/users', 'Admin\UsersController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
